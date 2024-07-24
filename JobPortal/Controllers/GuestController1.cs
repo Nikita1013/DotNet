8b99b690bcaf4b78a5ce7a27ba0c82980b24458a
Nikita1013 <shindenikita646@gmail.com>
2024-07-24 23:16:19 +0530

    Dotnet practice

---
+++ JobPortal/Controllers/GuestController1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobPortal.Controllers
+{
+    public class GuestController1 : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+        public IActionResult Career()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+
+    }
+}
